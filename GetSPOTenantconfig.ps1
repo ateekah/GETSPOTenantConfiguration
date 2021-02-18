dd0c2bc9f6ee54fb74a0c7ef9aa1b06c76d9d494
ateekah <58604757+ateekah@users.noreply.github.com>
2021-02-18 10:34:15 -0500

    Add files via upload

--- GetSPOTenantconfig.ps1
+++ GetSPOTenantconfig.ps1
@@ -1,7 +1,8 @@
 Write-Warning "This is only a test to gather SharePoint Online Tenant information"
-Write-Warning "TCheck that you have a C:\temp directory"
+Write-Warning "Check that you have a C:\temp directory"
 Write-Warning "Make sure you downloaded and installed the latest version of SharePoint Management Shell at 
 at https://www.microsoft.com/en-us/download/details.aspx?id=35588"
+Write-Warning "Do you need to delete the prior spotenantconfig.txt"
 
 Import-Module Microsoft.Online.SharePoint.PowerShell -DisableNameChecking
 
@@ -10,7 +11,7 @@ $SaveExecutionPolicy = Get-ExecutionPolicy
 Set-ExecutionPolicy RemoteSigned -Scope Currentuser
 
 #Connect to SPO
-Connect-SPOService -Url https://yoururl.sharepoint.com -credential yourglobaladminname@microsoft311.onmicrosoft.com
+Connect-SPOService -Url https://yourtenant.sharepoint.com -credential yourglobaladmin@yourtenant.onmicrosoft.com
 
 # View all of the other SPO management shell cmdlets by running the #following 
 #get-help *SPO* | where modulename –eq #microsoft.online.sharepoint.powershell
@@ -34,6 +35,19 @@ Write-Host "Sites size in MB"
 "SharePoint sites size in MB" | Out-File -FilePath c:\temp\spotenantconfig.txt -append
 Get-SPOSite -Limit All| select url, @{label="Size in MB";Expression={$_.usage.storage/1MB}} | Sort-Object -Descending -Property "Size in MB" | Format-Table –AutoSize >> c:\temp\spotenantconfig.txt 
 
+
+# Gets SPOsite
+Write-Host "Get-SPOSite"
+"Get-SPOSite" | Out-File -FilePath c:\temp\spotenantconfig.txt -append
+Get-SPOSite | select url, Title, template >> c:\temp\spotenantconfig.txt
+
+
+# Get sites with O365 Group
+Write-Host "Get-SPOSite with Group Template"
+"Get-SPOSite with Group Template" | Out-File -FilePath c:\temp\spotenantconfig.txt -append
+Get-SPOSite | select url, Title, template | where { $_.Template -eq "GROUP#0"} >> c:\temp\spotenantconfig.txt
+
+
 # Gets SPOUser
 Write-Host "Get-SPOUser"
 "All Users" | Out-File -FilePath c:\temp\spotenantconfig.txt -append
@@ -51,7 +65,7 @@ Write-Host "Get-SPOSiteGroup"
 "SPOSiteGroup" | Out-File -FilePath c:\temp\spotenantconfig.txt -append
 Get-SPOSiteGroup >> c:\temp\spotenantconfig.txt
 
-Write-Host "Completed. Thank you!" 
+Write-Host "Completed. Great Job! Thank you!" 
 Write-Host "Author: Anthony Teekah" 
 
 # Reset the execution policy to the original state
