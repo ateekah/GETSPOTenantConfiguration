44f4fc44ae2a94398b727fa94942f290d3cb19fd
ateekah <58604757+ateekah@users.noreply.github.com>
2022-04-19 10:58:11 -0400

    Update GetSPOTenantconfig.ps1

--- GetSPOTenantconfig.ps1
+++ GetSPOTenantconfig.ps1
@@ -17,6 +17,7 @@ Set-ExecutionPolicy RemoteSigned -Scope Currentuser
 
 #Connect to SPO
 Connect-SPOService -Url https://yourtenant-admin.sharepoint.com -credential yourglobaladmin@yourtenant.onmicrosoft.com
+#if MFA enabled the  run - Connect-SPOService -Url https://yourtenant-admin.sharepoint.com #-credential yourglobaladmin@yourtenant.onmicrosoft.com
 
 # View all of the other SPO management shell cmdlets by running the #following 
 #get-help *SPO* | where modulename –eq #microsoft.online.sharepoint.powershell
@@ -38,7 +39,7 @@ Get-SPOTenant >> c:\temp\spotenantconfig.txt
 # Gets the sites sizes in MB
 Write-Host "Sites size in MB" 
 "SharePoint sites size in MB" | Out-File -FilePath c:\temp\spotenantconfig.txt -append
-Get-SPOSite -Limit All| select url, @{label="Size in MB";Expression={$_.usage.storage/1MB}} | Sort-Object -Descending -Property "Size in MB" | Format-Table –AutoSize >> c:\temp\spotenantconfig.txt 
+Get-SPOSite -Limit All | select url, @{label="Size in MB";Expression={$_.usage.storage/1MB}} | Sort-Object -Descending -Property "Size in MB" | Format-Table –AutoSize >> c:\temp\spotenantconfig.txt 
 
 
 # Gets SPOsite
