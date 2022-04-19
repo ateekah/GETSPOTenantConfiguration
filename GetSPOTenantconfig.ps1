76935beb48ce606dbb5bdacb40b5e1d95ca545e1
ateekah <58604757+ateekah@users.noreply.github.com>
2022-04-19 10:42:18 -0400

    Update GetSPOTenantconfig.ps1

--- GetSPOTenantconfig.ps1
+++ GetSPOTenantconfig.ps1
@@ -1,3 +1,8 @@
+#Install SPO Management sgell
+#https://www.microsoft.com/en-us/download/details.aspx?id=35588
+# You need SPO admin account and account that is not MFA enabled
+
+
 Write-Warning "This is only a test to gather SharePoint Online Tenant information"
 Write-Warning "Check that you have a C:\temp directory"
 Write-Warning "Make sure you downloaded and installed the latest version of SharePoint Management Shell at 
@@ -14,7 +19,7 @@ Set-ExecutionPolicy RemoteSigned -Scope Currentuser
 Connect-SPOService -Url https://yourtenant.sharepoint.com -credential yourglobaladmin@yourtenant.onmicrosoft.com
 
 # View all of the other SPO management shell cmdlets by running the #following 
-#get-help *SPO* | where modulename –eq #microsoft.online.sharepoint.powershell
+#get-help *SPO* | where modulename â€“eq #microsoft.online.sharepoint.powershell
 
 
 # Gets the External users
@@ -33,7 +38,7 @@ Get-SPOTenant >> c:\temp\spotenantconfig.txt
 # Gets the sites sizes in MB
 Write-Host "Sites size in MB" 
 "SharePoint sites size in MB" | Out-File -FilePath c:\temp\spotenantconfig.txt -append
-Get-SPOSite -Limit All| select url, @{label="Size in MB";Expression={$_.usage.storage/1MB}} | Sort-Object -Descending -Property "Size in MB" | Format-Table –AutoSize >> c:\temp\spotenantconfig.txt 
+Get-SPOSite -Limit All| select url, @{label="Size in MB";Expression={$_.usage.storage/1MB}} | Sort-Object -Descending -Property "Size in MB" | Format-Table â€“AutoSize >> c:\temp\spotenantconfig.txt 
 
 
 # Gets SPOsite
