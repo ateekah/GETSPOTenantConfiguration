5a53e08c94d78b65473725291e046086a1e196ba
ateekah <58604757+ateekah@users.noreply.github.com>
2022-12-07 11:29:06 -0500

    Add files via upload

---
+++ sharepointsites.ps1
@@ -0,0 +1,21 @@
+﻿Import-Module Microsoft.Online.Sharepoint.PowerShell -DisableNameChecking
+ 
+$AdminSiteURL="https://m365x31609423-admin.sharepoint.com"
+ 
+$Credential = Get-credential
+Connect-SPOService -url $AdminSiteURL -Credential $Credential
+ 
+#sharepoint online list all site collections powershell
+Get-SPOSite -Detailed | Format-Table Url, Template, StorageUsageCurrent, StorageQuota, LastContentModifiedDate -AutoSize 
+
+pause
+
+#sort by size
+Get-SPOSite -Detailed | select Url, StorageUsageCurrent, LastContentModifiedDate | Sort-Object StorageUsageCurrent –Descending 
+
+pause
+#sort by date
+Get-SPOSite -Detailed | select Url, StorageUsageCurrent, LastContentModifiedDate | Sort-Object LastContentModifiedDate –Descending
+
+
+#Read more: https://www.sharepointdiary.com/2016/02/get-all-site-collections-in-sharepoint-online-using-powershell.html#ixzz7miav0pXI
