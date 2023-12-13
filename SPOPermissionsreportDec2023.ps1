9ead25c0fdcfffcf0f4a2e810fe5f7bce27bf803
ateekah <58604757+ateekah@users.noreply.github.com>
2023-12-13 17:16:52 -0500

    Add files via upload

---
+++ SPOPermissionsreportDec2023.ps1
@@ -0,0 +1,35 @@
+#Finds Group name, permissions, and members of that group
+#Import SharePoint Online Management Shell
+Import-Module Microsoft.Online.Sharepoint.PowerShell -DisableNameChecking
+
+#Variables for SharePoint Online Admin & Target site collection
+$AdminSiteURL="https://microsoft311dev-admin.sharepoint.com/"
+$SiteCollURL="https://microsoft311dev.sharepoint.com/"
+
+#Get the Credentials
+$Credential = Get-credential
+#Connect To SharePoint Online
+Connect-SPOService -url $AdminSiteURL -Credential $Credential
+
+#Get the Site collection
+$Site = Get-SPOSite $SiteCollURL
+
+#Get all Groups of the site collection    
+$GroupColl = Get-SPOSiteGroup -Site $Site | Where { $_.Roles -ne $NULL -and $_.Users -ne $NULL}
+
+Foreach($Group in $GroupColl)
+{
+    #Get Permissions assigned to the Group
+    $GroupPermissions=""
+    ForEach($Role in $Group.Roles)
+    {
+        $GroupPermissions+= $Role+";"
+    }
+    Write-host -f Yellow "Group Name: $($Group.Title) - Permissions: $($GroupPermissions)"
+
+    #Get each member of the group
+    foreach($User in $Group.Users)
+    {
+         write-host -f Green $user
+    }              
+}
